38b0a3b668e34d56934f044805b4a7edfcf6d6e4
lipeiyang8 <1104468215@qq.com>
2019-12-06 16:25:58 +0800

    api

---
+++ com-demo-api/src/main/java/com/demo/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * UserApplication实体
+ *
+ * @author Faker
+ * @version 1.0
+ * @since 2019-12-06 10:29:34
+ */
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
