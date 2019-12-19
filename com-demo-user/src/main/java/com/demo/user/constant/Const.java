5e11f78669c65f29af05ca94aba3e2d188642749
faker <zhanglele@haodf.com>
2019-12-19 11:00:38 +0800

    bugfix.

--- com-demo-user/src/main/java/com/demo/user/constant/Const.java
+++ com-demo-user/src/main/java/com/demo/user/constant/Const.java
@@ -1,17 +1,7 @@
 package com.demo.user.constant;
 
 /**
- * <p>
  * 常量池
- * </p>
- *
- * @package: com.xkcoding.orm.jdbctemplate.constant
- * @description: 常量池
- * @author: yangkai.shen
- * @date: Created in 2018/10/15 10:59 AM
- * @copyright: Copyright (c) 2018
- * @version: V1.0
- * @modified: yangkai.shen
  */
 public interface Const {
 	/**
